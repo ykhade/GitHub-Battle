de1d03ecfd26ac1149d6894f82f241368a8ea418
yashkhade <ykhade@nevada.unr.edu>
2019-02-13 00:51:18 -0800

    Added props to pass information to componants

--- app/index.js
+++ app/index.js
@@ -4,15 +4,43 @@ require('./index.css');
 
 class App extends React.Component {
 	render() {
+		var friends = this.props.list.filter(function(user){
+            return user.friend === true
+          });
+    var notFriends = this.props.list.filter(function(user){
+            return user.friend !== true
+          });
 		return (
 			<div>
-			  Hello Yash
-			</div>
+        	<h1>Friends</h1>
+        	<ul>
+          		{friends.map(function(user){
+            		return <li key = {user.name}>{user.name}</li>
+          		})
+          		}
+        	</ul>
+        
+        	<hr />
+        
+        	<h1> Non Friends </h1>
+        	<ul>
+          		{notFriends.map(function(user){
+            		return <li key = {user.name}>{user.name}</li>
+          		}	)}
+        	</ul>        
+      		</div>
 		)
 	}
 }
 
 ReactDOM.render(
-  <App />,
+  <App list={[
+  	{ name: 'Tyler', friend: true },
+    { name: 'Ryan', friend: true },
+    { name: 'Michael', friend: false },
+    { name: 'Mikenzi', friend: false },
+    { name: 'Jessica', friend: true },
+    { name: 'Dan', friend: false } ]}
+  />,
   document.getElementById('app')
 )
