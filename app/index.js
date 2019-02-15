3e91d2925b190271b4613616ef9a942953425e78
yashkhade <ykhade@nevada.unr.edu>
2019-02-14 19:14:42 -0800

    Added language choice box

--- app/index.js
+++ app/index.js
@@ -1,46 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
-
-class App extends React.Component {
-	render() {
-		var friends = this.props.list.filter(function(user){
-            return user.friend === true
-          });
-    var notFriends = this.props.list.filter(function(user){
-            return user.friend !== true
-          });
-		return (
-			<div>
-        	<h1>Friends</h1>
-        	<ul>
-          		{friends.map(function(user){
-            		return <li key = {user.name}>{user.name}</li>
-          		})
-          		}
-        	</ul>
-        
-        	<hr />
-        
-        	<h1> Non Friends </h1>
-        	<ul>
-          		{notFriends.map(function(user){
-            		return <li key = {user.name}>{user.name}</li>
-          		}	)}
-        	</ul>        
-      		</div>
-		)
-	}
-}
+var App = require('./components/App')
 
 ReactDOM.render(
-  <App list={[
-  	{ name: 'Tyler', friend: true },
-    { name: 'Ryan', friend: true },
-    { name: 'Michael', friend: false },
-    { name: 'Mikenzi', friend: false },
-    { name: 'Jessica', friend: true },
-    { name: 'Dan', friend: false } ]}
-  />,
-  document.getElementById('app')
+    <App/>,
+    document.getElementById('app')
 )
