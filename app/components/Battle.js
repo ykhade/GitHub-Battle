f2d406dcea8f7e7553ba5c03b3b1f1fc42c61f51
yashkhade <ykhade@nevada.unr.edu>
2019-02-15 19:06:18 -0800

    Added a playerpreview file and Loading file to abstract the functions.

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,38 +1,11 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var Link = require('react-router-dom').Link;
-
-function PlayerPreview(props) {
-    return (
-        <div>
-            <div className='column'>
-                <img
-                    className='avatar'
-                    src={props.avatar}
-                    alt={'Avatar for ' + props.username}
-                />
-                <h2 className='username'>@{props.username}</h2>
-            </div>
-            <button
-                className='reset'
-                onClick={props.onReset.bind(null, props.id)}>
-                Reset
-            </button>
-
-        </div>
-    )
-}
-
-PlayerPreview.propTypes = {
-    avatar: PropTypes.string.isRequired,
-    username: PropTypes.string.isRequired,
-    onReset: PropTypes.func.isRequired
-};
+var PlayerPreview = require('./PlayerPreview');
 
 class PlayerInput extends React.Component {
     constructor(props) {
         super(props);
-
         this.state = {
             username: ''
         };
@@ -48,7 +21,6 @@ class PlayerInput extends React.Component {
             return {
                 username: value
             }
-
         });
     }
 
@@ -61,19 +33,16 @@ class PlayerInput extends React.Component {
         );
     }
 
-
     render() {
         return (
             <form className='column' onSubmit={this.handleSubmit}>
-                <label className='header' htmlFor='username'>
-                    {this.props.label}
-                </label>
+                <label className='header' htmlFor='username'>{this.props.label}</label>
                 <input
                     id='username'
                     placeholder='github username'
                     type='text'
-                    autoComplete='off'
                     value={this.state.username}
+                    autoComplete='off'
                     onChange={this.handleChange}
                 />
                 <button
@@ -108,14 +77,13 @@ class Battle extends React.Component {
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleReset = this.handleReset.bind(this);
     }
 
     handleSubmit(id, username) {
         this.setState(function () {
             var newState = {};
             newState[id + 'Name'] = username;
-            newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200';
+            newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200'
             return newState;
         });
     }
@@ -129,40 +97,51 @@ class Battle extends React.Component {
         })
     }
 
-
     render() {
         var match = this.props.match;
         var playerOneName = this.state.playerOneName;
-        var playerTwoName = this.state.playerTwoName;
         var playerOneImage = this.state.playerOneImage;
+        var playerTwoName = this.state.playerTwoName;
         var playerTwoImage = this.state.playerTwoImage;
+
         return (
             <div>
                 <div className='row'>
-                    {!playerOneName && <PlayerInput
+                    {!playerOneName &&
+                    <PlayerInput
                         id='playerOne'
                         label='Player One'
-                        onSubmit={this.handleSubmit}/>
-                    }
+                        onSubmit={this.handleSubmit}
+                    />}
+
                     {playerOneImage !== null &&
                     <PlayerPreview
                         avatar={playerOneImage}
-                        username={playerOneName}
-                        id='playerOne'
-                        onReset={this.handleReset}
-                    />}
-                    {!playerTwoName && <PlayerInput
+                        username={playerOneName}>
+                        <button
+                            className='reset'
+                            onClick={this.handleReset.bind(this, 'playerOne')}>
+                            Reset
+                        </button>
+                    </PlayerPreview>}
+
+                    {!playerTwoName &&
+                    <PlayerInput
                         id='playerTwo'
                         label='Player Two'
-                        onSubmit={this.handleSubmit}/>
-                    }
+                        onSubmit={this.handleSubmit}
+                    />}
+
                     {playerTwoImage !== null &&
                     <PlayerPreview
                         avatar={playerTwoImage}
-                        username={playerTwoName}
-                        id='playerTwo'
-                        onReset={this.handleReset}
-                    />}
+                        username={playerTwoName}>
+                        <button
+                            className='reset'
+                            onClick={this.handleReset.bind(this, 'playerTwo')}>
+                            Reset
+                        </button>
+                    </PlayerPreview>}
                 </div>
 
                 {playerOneImage && playerTwoImage &&
@@ -170,14 +149,13 @@ class Battle extends React.Component {
                     className='button'
                     to={{
                         pathname: match.url + '/results',
-                        search: '?playerOneName' + playerOneName + '?playerTwoName' + playerTwoName
+                        search: '?playerOneName=' + playerOneName + '&playerTwoName=' + playerTwoName
                     }}>
                     Battle
                 </Link>}
             </div>
         )
     }
-
 }
 
 module.exports = Battle;
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         historyApiFallback: true
     },
 
-    mode: 'development',
+    mode: process.env.NODE_ENV ==='production' ? 'production' : 'development',
     plugins: [
         new HtmlWebpackPlugin({
             template: 'app/index.html'
