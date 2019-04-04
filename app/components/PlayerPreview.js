c50a1615bf396e929fceafc3879886525b222242
yashkhade <ykhade@nevada.unr.edu>
2019-04-03 18:19:43 -0700

    Refactored to ES6

--- app/components/PlayerPreview.js
+++ app/components/PlayerPreview.js
@@ -1,17 +1,18 @@
-var React = require('react');
-var PropTypes = require('prop-types');
+const React = require('react');
+const PropTypes = require('prop-types');
 
-function PlayerPreview(props) {
-    return(
+function PlayerPreview ({ avatar, username, children }) {
+    return (
         <div>
             <div className='column'>
                 <img
                     className='avatar'
-                    src={props.avatar}
-                    alt={'Avatar for ' + props.username}/>
-                <h2 className='username'>@{props.username}</h2>
+                    src={avatar}
+                    alt={'Avatar for ' + username}
+                />
+                <h2 className='username'>@{username}</h2>
             </div>
-            {props.children}
+            {children}
         </div>
     )
 }
